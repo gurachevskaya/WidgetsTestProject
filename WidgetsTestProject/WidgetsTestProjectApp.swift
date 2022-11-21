5bd9d38b90871682ba4fa288bbf83e8afce25273
Karina Gurachevskaya <k.gurachevskaya@gmail.com>
2022-11-21 10:43:37 +0100

    Project init

---
+++ WidgetsTestProject/WidgetsTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsTestProjectApp.swift
+//  WidgetsTestProject
+//
+//  Created by Karina gurachevskaya on 18.11.22.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
